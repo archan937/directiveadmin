321cc07b0d84877e9f5efdf53b272960aaf4d014
Paul Engel <pm_engel@icloud.com>
2014-12-09 17:42:43 +0100

    Not instantiating scoped collection

--- lib/directive_admin/gem_ext/active_admin/views/pages/base.rb
+++ lib/directive_admin/gem_ext/active_admin/views/pages/base.rb
@@ -3,14 +3,18 @@ module ActiveAdmin
     module Pages
       class Base < Arbre::HTML::Document
 
+        alias :original_panel :panel
+
+        def panel(*args, &block)
+          options = args.extract_options!
+          return unless options.delete(:details) ? authorized?(:details, resource) : authorized?(:panel, args[0], nil, (args.size == 2 ? args[1] : true))
+          original_panel *args.slice(0, 1).push(options), &block
+        end
+
         def collection_panel(name, &block)
           collection = scope resource.send(name)
           klass = resource.send(name).klass
           label = name.to_s.humanize
-
-          unless block_given?
-            block = DirectiveAdmin.namespace.resources.detect{|x| (x.resource_class rescue nil) == klass}.page_presenters[:index][:table].block
-          end
 
           panel "#{label} (#{collection.size})" do
             if collection.empty?
@@ -21,18 +25,8 @@ module ActiveAdmin
           end
         end
 
-        alias :original_panel :panel
-
-        def panel(*args, &block)
-          options = args.extract_options!
-          return unless options.delete(:details) ? authorized?(:details, resource) : authorized?(:panel, args[0], nil, (args.size == 2 ? args[1] : true))
-          original_panel *args.slice(0, 1).push(options), &block
-        end
-
         def scope(collection)
-          klass = collection.klass
-          scoped_collection = active_admin_authorization.scope_collection(collection)
-          klass.instantiate_all scoped_collection.qry_options[:where], scoped_collection
+          active_admin_authorization.scope_collection(collection)
         end
 
       end
