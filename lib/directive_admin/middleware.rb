ff78ecaed2a2ff513e20430dd8f5fdba38a7f162
Paul Engel <pm_engel@icloud.com>
2015-02-26 13:36:01 +0100

    Fixed redirect loop

--- lib/directive_admin/middleware.rb
+++ lib/directive_admin/middleware.rb
@@ -7,9 +7,10 @@ module DirectiveAdmin
 
     def call(env)
       if current_user_method = ActiveAdmin.application.current_user_method
+        regex = /^\/impersonate\/(\d+)/
         user_scope = current_user_method.to_s.gsub("current_", "").to_sym
 
-        if env["PATH_INFO"].match(/^\/impersonate\/(\d+)/)
+        if env["PATH_INFO"].match(regex)
           impersonated_user_id = $1.to_i
           actual_user = env["warden"].authenticate(:scope => user_scope)
 
@@ -18,8 +19,13 @@ module DirectiveAdmin
           end
 
           env["rack.session"][:"impersonated_#{user_scope}_id"] = impersonated_user_id
+          location = Rack::Request.new(env).referrer
 
-          return [302, {"Location" => (Rack::Request.new(env).referrer || "/#{DirectiveAdmin.namespace.name}")}, ["Moved Temporarily"]]
+          if location.nil? || URI.parse(location).path.match(regex)
+            location = "/#{DirectiveAdmin.namespace.name}"
+          end
+
+          return [302, {"Location" => location}, ["Moved Temporarily"]]
         end
       end
       @app.call env
