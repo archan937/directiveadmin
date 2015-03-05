f2f00225e401ec957221c15b24879b03ec6b068b
Paul Engel <pm_engel@icloud.com>
2015-03-05 12:29:44 +0100

    Disabled turbolinks for impersonation menu

--- lib/directive_admin/gem_ext/active_admin/namespace.rb
+++ lib/directive_admin/gem_ext/active_admin/namespace.rb
@@ -3,7 +3,7 @@ module ActiveAdmin
 
     def add_impersonable_users_to_menu(menu, current_user)
       (current_user.class.where("id != #{current_user.id}")).each do |user|
-        menu.add id: "impersonate_#{user.id}", label: ->{ display_name user }, url: "/impersonate/#{user.id}", parent: "current_user"
+        menu.add id: "impersonate_#{user.id}", label: ->{ display_name user }, url: "/impersonate/#{user.id}", parent: "current_user", html_options: {data: {no_turbolink: true}}
       end
     end
 
