91deb108e5e2211ebf86892eed1f069b3208411f
Paul Engel <pm_engel@icloud.com>
2015-05-01 12:00:15 +0200

    Being able to select a namespace

--- lib/directive_admin/gem_ext/active_admin/application.rb
+++ lib/directive_admin/gem_ext/active_admin/application.rb
@@ -2,6 +2,18 @@ module ActiveAdmin
   class Application
 
     inheritable_setting :route_prefix, nil
+    attr_reader :selected_namespace
+
+    def select_namespace!(namespace)
+      self.default_namespace = @selected_namespace = namespace.to_sym
+    end
+
+    alias_method :original_namespaces, :namespaces
+
+    def namespaces
+      original_namespaces.select!{|k, v| k.to_sym == selected_namespace} if selected_namespace
+      original_namespaces
+    end
 
     def all_javascripts
       namespaces.inject(Set.new) do |js, (name, namespace)|
