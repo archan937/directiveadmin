0d490b4ce6f2672fa43a20bf76f43a99e2f10f9e
Paul Engel <pm_engel@icloud.com>
2015-05-28 18:03:28 +0200

    Being able to ask whether or not scope is applied

--- lib/directive_admin/gem_ext/active_admin/base_controller.rb
+++ lib/directive_admin/gem_ext/active_admin/base_controller.rb
@@ -1,1 +1,15 @@
 require "directive_admin/gem_ext/active_admin/base_controller/authorization"
+
+module ActiveAdmin
+  class BaseController < ::InheritedResources::Base
+
+    helper_method :scope?
+
+  protected
+
+    def scope?(key, klass = resource_class)
+      active_admin_authorization.send(:policy, klass, nil).class::Scope.new(send(ActiveAdmin.application.current_user_method), nil).scope?(key)
+    end
+
+  end
+end
