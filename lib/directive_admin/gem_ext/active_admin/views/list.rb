63d1dd0aa23b16d6441d0ff986659788b2020d57
Paul Engel <pm_engel@icloud.com>
2015-07-29 17:22:26 +0200

    Improved rendering lists on the show page: - Being able to suppress the “Create” link - Corrected unnecessary displayed hairline - HTML template not required - respecting (optional) association ordering

--- lib/directive_admin/gem_ext/active_admin/views/list.rb
+++ lib/directive_admin/gem_ext/active_admin/views/list.rb
@@ -4,7 +4,7 @@ module ActiveAdmin
 
       builder_method :list
 
-      def build(title, &block)
+      def build(title, options = {}, &block)
         name = title.underscore
         return unless authorized?(:list, name, resource.class)
 
@@ -21,23 +21,31 @@ module ActiveAdmin
         collection = active_admin_authorization.scope_collection(resource.send(name))
         klass = collection.klass
         default_attributes = Hash[klass.column_names.zip]
-        qry_options = collection.qry_options(*@select).merge(:group_by => "id", :order_by => keys)
+        qry_options = collection.qry_options(*@select).merge(:group_by => "id").reverse_merge(:order_by => keys)
 
         collection = klass.connection.select_all(klass.to_qry(qry_options)).group_by{|x| x.values_at(*keys)}.values.collect do |data|
           attributes = data.first
           klass.instantiate default_attributes.merge(attributes)
         end
 
+        create = (options[:create] != false) && authorized?(:create, klass)
+
         add_class "panel"
         h3 "#{title} (#{collection.size})"
         div class: "panel_contents" do
-          ul :class => name, :"data-parent" => "#{resource.class.reflect_on_association(name.to_sym).foreign_key}##{resource.id}" do
+          ul :class => "#{name} #{"no-create" unless create}".strip, :"data-parent" => "#{resource.class.reflect_on_association(name.to_sym).foreign_key}##{resource.id}" do
             key = name.singularize.to_sym
-            collection.each do |record|
-              render partial: "#{name}/show", locals: {key => record}
+            begin
+              collection.each do |record|
+                render partial: "#{name}/show", locals: {key => record}
+              end
+            rescue ActionView::MissingTemplate
+              collection.each do |record|
+                li keys.collect{|attribute| record.send(attribute)}.join(" / ")
+              end
             end
           end
-          if authorized? :create, klass
+          if create
             div :class => "container" do
               a :href => "#", :class => "create", :"data-no-turbolink" => "true" do
                 "Create a#{"n" if %w(a e i u o).include? name[0]} #{name.singularize}"
